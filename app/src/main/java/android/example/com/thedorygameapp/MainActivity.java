367f84bca1ff942644167f4b06cf15191149f76f
MoulikaB <moulikaprasad1@gmail.com>
2019-01-06 16:11:47 -0500

    Android Gmae Project

---
+++ app/src/main/java/android/example/com/thedorygameapp/MainActivity.java
@@ -0,0 +1,34 @@
+package android.example.com.thedorygameapp;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DoryFishView gameView;
+    private Handler handler = new Handler();
+    private final static long Interval = 30;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       gameView = new DoryFishView(this);
+       setContentView(gameView);
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        gameView.invalidate();
+                    }
+                });
+            }
+        },0, Interval);
+    }
+}
